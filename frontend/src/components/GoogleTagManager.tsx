689385af442d5068be692b84d2d36d3ba1afc24a
Sergii Mavrov <serg.mavrov@gmail.com>
2026-02-20 09:48:24 +0100

    fix: replace inline style with hidden attribute in GTM noscript iframe

--- frontend/src/components/GoogleTagManager.tsx
+++ frontend/src/components/GoogleTagManager.tsx
@@ -34,7 +34,7 @@ export function GoogleTagManagerNoScript() {
                 src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
                 height="0"
                 width="0"
-                style={{ display: 'none', visibility: 'hidden' }}
+                hidden
             />
         </noscript>
     );
